bc442a1df947228e844f62e0d0eced3c4d0acfa8
Сарангэрэл Хижигдорж <HP231087@G303-CC0131.local>
2024-08-05 15:17:11 +0800

    please

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -12,14 +12,74 @@ import { SignUp } from "@/components/subComponents/signUp";
 import { LogAndSign } from "@/components/subComponents/LogAndSign";
 import { PasswordConfirm } from "@/components/subComponents/passwordConfirm";
 
+import {
+  collection,
+  doc,
+  getDoc,
+  getDocs,
+  onSnapshot,
+  query,
+} from "firebase/firestore";
+import { db } from "@/common";
+
 function HomeScreen({ navigation }: { navigation: any }) {
+  const getPostsAndUserInfo = async () => {
+    const q = query(collection(db, "posts"));
+
+    onSnapshot(q, async (snapshot) => {
+      console.log(snapshot, "-");
+      const userPromises = snapshot.docs.map((postDoc) => {
+        const postData = postDoc.data();
+        console.log(postData);
+        const userRef = postData.userRef;
+        console.log(userRef);
+        return getDoc(userRef)
+          .then((userDoc) => {
+            if (userDoc.exists()) {
+              return {
+                post: postData,
+                user: userDoc.data(),
+              };
+            }
+          })
+          .catch((error) => {
+            console.error(error);
+            return {
+              post: postData,
+              user: null,
+            };
+          });
+      });
+      console.log(console.log(userPromises, "--"));
+      try {
+        // const postsWithUserInfo = await Promise.all(userPromises);
+        // console.log(postsWithUserInfo);
+      } catch (error) {
+        console.error(error);
+      }
+    });
+  };
+
   return (
-    <View>
+    <View style={{ backgroundColor: "grey", height: "100%" }}>
       <Pressable
         onPress={() => navigation.navigate("SignUp")}
         style={{ top: 100, backgroundColor: "pink" }}
       >
         <Text style={{ color: "white", top: 100 }}>oncluf</Text>
+      </Pressable>
+      <Pressable
+        onPress={() => navigation.navigate("Login")}
+        style={{ top: 100, backgroundColor: "green" }}
+      >
+        <Text style={{ color: "white", top: 100 }}>Log in</Text>
+      </Pressable>
+
+      <Pressable
+        onPress={() => getPostsAndUserInfo()}
+        style={{ top: 100, backgroundColor: "yellow" }}
+      >
+        <Text style={{ color: "white", top: 100 }}>Log in</Text>
       </Pressable>
     </View>
   );

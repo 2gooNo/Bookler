3ce1a2f66c34435e257e4457f0d3cc652eff939e
2gooNo <143684446+2gooNo@users.noreply.github.com>
2024-08-12 17:46:39 +0800

    book create

--- app/(tabs)/books.tsx
+++ app/(tabs)/books.tsx
@@ -1,18 +1,31 @@
-import Ionicons from "@expo/vector-icons/Ionicons";
-import { StyleSheet, Image, Platform, View } from "react-native";
+"use client";
 
+import Ionicons from "@expo/vector-icons/Ionicons";
+import { StyleSheet, Image, Platform, View, Pressable } from "react-native";
 import { Collapsible } from "@/components/Collapsible";
 import { ExternalLink } from "@/components/ExternalLink";
-import ParallaxScrollView from "@/components/ParallaxScrollView";
-import { ThemedText } from "@/components/ThemedText";
-import { ThemedView } from "@/components/ThemedView";
-import { Camera } from "@/components/subComponents/Camera";
+import { useContext, useEffect, useState } from "react";
+import { useRoute } from "@react-navigation/native";
+import { useNavigation } from "expo-router";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import { Formik } from "formik";
+import { CreateBook } from "@/components/subComponents/CreateBook";
 
-export default function BookCommunities() {
+export function BookCommunities({ navigation }: { navigation: any }) {
   return (
     <View
-      style={{ backgroundColor: "green", width: "100%", height: "100%" }}
-    ></View>
+      style={{
+        width: "100%",
+        height: "100%",
+        alignItems: "center",
+        paddingTop: "50%",
+      }}
+    >
+      <Pressable
+        style={{ backgroundColor: "white", width: "20%", height: "15%" }}
+        onPress={() => navigation.navigate("CreateBook")}
+      ></Pressable>
+    </View>
   );
 }
 
@@ -28,3 +41,39 @@ const styles = StyleSheet.create({
     gap: 8,
   },
 });
+const HomeStack = createNativeStackNavigator();
+
+export default function HomeStackScreen() {
+  const navigation = useNavigation();
+  const route = useRoute();
+
+  useEffect(() => {
+    const routeIndex = navigation?.getState()?.routes?.[5].state?.index;
+    const shouldHideTabBar = routeIndex === 1;
+    navigation.setOptions({
+      tabBarStyle: {
+        display: shouldHideTabBar ? "none" : "flex",
+      },
+    });
+  }, [navigation, route]);
+  return (
+    <HomeStack.Navigator>
+      <HomeStack.Screen
+        name="BookCommunities"
+        component={BookCommunities}
+        options={{ headerShown: false }}
+      />
+
+      <HomeStack.Screen
+        name="CreateBook"
+        component={CreateBook}
+        options={{
+          animation: "slide_from_bottom",
+          headerShown: false,
+          gestureDirection: "vertical",
+          presentation: "modal",
+        }}
+      />
+    </HomeStack.Navigator>
+  );
+}
